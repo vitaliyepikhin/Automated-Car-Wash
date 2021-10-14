f0f9c46ac4447deadbb343f85061ce7d7e90f3ce
vitaliyepikhin <22.vint7@gmail.com>
2021-10-14 09:31:06 +0300

    14.10.21 9:31

---
+++ Controllers/Client_Controller.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Automated_Car_Wash.Controllers
+{
+    [ApiController]
+    [Route("/Client")]
+    public class Client_Controller : ControllerBase
+    {
+
+        [HttpGet("DistSlezhka")]
+        public string DistSlezhka(string str)
+        {
+            return str; //"Метод слежки за процессом мойки"
+        }
+
+        [HttpGet("ZakazUslug")]
+        public string ZakazUslug(string str)
+        {
+            return str;// "Метод заказа услуг"
+        }
+
+        [HttpGet("OcenkaKachestva")]
+        public string OcenkaKachestva(string str)
+        {
+            return str;// "Метод оценки качества"
+        }
+
+        [HttpGet("Oplata")]
+        public string Oplata(string str)
+        {
+            return str;// "Метод оплаты мойки"
+        }
+
+    }
+}
---
+++ Controllers/Operator_Controller.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Automated_Car_Wash.Controllers
+{
+    [ApiController]
+    [Route("/Operator")]
+    public class Operator_Controller : ControllerBase
+    {
+
+        [HttpGet("DistSlezhka")]
+        public string DistSlezhka(string str)
+        {
+            return str; //"Метод слежки за процессом мойки"
+        }
+
+        [HttpGet("PriemZakaza")]
+        public string PriemZakaza(string str)
+        {
+            return str;// "Метод приёма заказа"
+        }
+
+        [HttpGet("ZapuskMoiki")]
+        public string ZapuskMoiki(string str)
+        {
+            return str;// "Метод запуска робот-мойки"
+        }
+
+        [HttpGet("PriemOplata")]
+        public string PriemOplata(string str)
+        {
+            return str;// "Метод приема оплаты"
+        }
+
+    }
+}
